749a2d67397db9c45004617d5178694a1d9f10c3
AYU-HH <ak5070@srmist.edu.in>
2026-03-02 13:10:17 +0530

    Create l2q3.java

---
+++ l2q3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class l2q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double perimeter = input.nextDouble();
+        double side = perimeter / 4.0;
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+        input.close();
+    }
+}
