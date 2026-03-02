38437f97d1032fbc88dc64d864fc6023fa8a9f5e
AYU-HH <ak5070@srmist.edu.in>
2026-03-02 13:05:11 +0530

    Create l1q3.java

---
+++ l1q3.java
@@ -0,0 +1,7 @@
+public class l1q3 {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
