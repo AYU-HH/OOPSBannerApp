d8190ee2ba565c45a887fc2e374c75ec01a053f9
AYU-HH <ak5070@srmist.edu.in>
2026-03-02 12:58:15 +0530

    Update LEVEL 1 Q1.java

--- LEVEL 1 Q1.java
+++ LEVEL 1 Q1.java
@@ -3,7 +3,6 @@ public class HarryAgeCalculator {
         int birthYear = 2000;
         int currentYear = 2024;
         int age = currentYear - birthYear;
-
         System.out.println("Harry's age in 2024 is " + age);
     }
 }
